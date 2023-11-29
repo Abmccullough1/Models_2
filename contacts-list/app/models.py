5c9f51248c0842bca9e735c0af106956fa5d3fa2
Abmccullough1 <wmccullough24@basecampcodingacademy.com>
2023-11-29 09:46:37 -0600

    hey i pushed this time still not sure how that happend

--- contacts-list/app/models.py
+++ contacts-list/app/models.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 
 # Create your models here.
-class Contact(models.Model):
+class Contacts(models.Model):
     name = models.TextField()
     email = models.TextField()
     phone = models.TextField()
@@ -11,35 +11,35 @@ class Contact(models.Model):
 
 
 def create(name, email, phone, favorite) -> Contact:
-    contact = Contact(name=name, email=email, phone=phone, is_favorite=favorite)
+    contact = Contacts(name=name, email=email, phone=phone, is_favorite=favorite)
     contact.save()
     return contact
 
 
-def favorite() -> Contact:
-    contacts = Contact.objects.filter(is_favorite=True)
+def favorite() -> Contacts:
+    contacts = Contacts.objects.filter(is_favorite=True)
     return contacts
 
 
-def all() -> Contact:
-    contacts = Contact.objects.all()
+def all() -> Contacts:
+    contacts = Contacts.objects.all()
     return contacts
 
 
-def update_email(name, email_new) -> Contact:
-    contact = Contact.objects.get(name=name)
+def update_email(name, email_new) -> Contacts:
+    contact = Contacts.objects.get(name=name)
     contact.email = email_new
     contact.save()
     return contact
 
-def find_by_name(name) -> Optional[Contact]:
+def find_by_name(name) -> Optional[Contacts]:
     try:
-        contacts = Contact.objects.get(name=name)
+        contacts = Contacts.objects.get(name=name)
         return contacts
     except:
         return None
 
 def delete(name):
-    contact = Contact.objects.get(name=name)
+    contact = Contacts.objects.get(name=name)
     contact.delete()
     return contact
