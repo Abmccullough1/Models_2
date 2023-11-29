1287170026132443eb1df74fda6f1dcd9e9e87ec
Abmccullough1 <wmccullough24@basecampcodingacademy.com>
2023-11-29 09:47:12 -0600

    hey i pushed this time still not sure how that happend

--- contacts-list/app/models.py
+++ contacts-list/app/models.py
@@ -8,37 +8,28 @@ class Contacts(models.Model):
     email = models.TextField()
     phone = models.TextField()
     is_favorite = models.BooleanField()
-
-
+    
 def create(name, email, phone, favorite) -> Contact:
     contact = Contacts(name=name, email=email, phone=phone, is_favorite=favorite)
     contact.save()
     return contact
-
-
 def favorite() -> Contacts:
     contacts = Contacts.objects.filter(is_favorite=True)
     return contacts
-
-
 def all() -> Contacts:
     contacts = Contacts.objects.all()
     return contacts
-
-
 def update_email(name, email_new) -> Contacts:
     contact = Contacts.objects.get(name=name)
     contact.email = email_new
     contact.save()
     return contact
-
 def find_by_name(name) -> Optional[Contacts]:
     try:
         contacts = Contacts.objects.get(name=name)
         return contacts
     except:
         return None
-
 def delete(name):
     contact = Contacts.objects.get(name=name)
     contact.delete()
